05b8df592757bc87fa19188a0fdc563ce7fa7442
vincentroman <vincentroman1@outlook.com>
2018-11-19 10:14:03 +0000

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,9 +3,38 @@
 /**
  * Catch All
  */
-server.opts(/\.*/, function(req, res, next) {
-	res.send(200);
-	next();
+server.get(/^\/([a-zA-Z0-9_\.~-]+)\/(.*)/, function(req, res, next) {
+  console.log(req.params[0]);
+  console.log(req.params[1]);
+  res.send(200);
+  return next();
+});
+
+server.post(/^\/([a-zA-Z0-9_\.~-]+)\/(.*)/, function(req, res, next) {
+  console.log(req.params[0]);
+  console.log(req.params[1]);
+  res.send(200);
+  return next();
+});
+
+server.del(/^\/([a-zA-Z0-9_\.~-]+)\/(.*)/, function(req, res, next) {
+  console.log(req.params[0]);
+  console.log(req.params[1]);
+  res.send(200);
+  return next();
+});
+
+server.patch(/^\/([a-zA-Z0-9_\.~-]+)\/(.*)/, function(req, res, next) {
+  console.log(req.params[0]);
+  console.log(req.params[1]);
+  res.send(200);
+  return next();
+});
+server.put(/^\/([a-zA-Z0-9_\.~-]+)\/(.*)/, function(req, res, next) {
+  console.log(req.params[0]);
+  console.log(req.params[1]);
+  res.send(200);
+  return next();
 });
 
 /**
